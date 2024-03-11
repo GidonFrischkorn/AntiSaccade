83482165a0f183e7e7bf4854aed610e1f3115854
GidonFrischkorn <gidon.frischkorn@uzh.ch>
2024-03-11 19:00:19 +0100

    Include Data of all SaccadeExps

---
+++ R/data.R
@@ -0,0 +1,236 @@
+#' Raw Data of the first Saccade Experiment
+#'
+#' @format ## `RawData_Exp1`
+#' A data frame with 25920 rows and 13 columns:
+#' \describe{
+#'   \item{Exp}{Label for the Experiment in the whole experiment series}
+#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
+#'   \item{Block}{The experimental block a trial was part of.}
+#'   \item{trial}{The trial number of the current trial}
+#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
+#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
+#'   "opposite" = the cue was presented opposite of the stimulus}
+#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
+#'   \item{CTI}{The cue target intercal coded in milliseconds.}
+#'   \item{stim}{The letter stimulus that was presented to be identified.}
+#'   \item{response}{The response given by the subject as the letter on the keyboard.}
+#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
+#'   \item{RTms}{Reaction Time of the response in milliseconds}
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"RawData_Exp1"
+
+#' Raw Data of the second Saccade Experiment
+#'
+#' @format ## `RawData_Exp2`
+#' A data frame with 35280 rows and 13 columns:
+#' \describe{
+#'   \item{Exp}{Label for the Experiment in the whole experiment series}
+#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
+#'   \item{Block}{The experimental block a trial was part of.}
+#'   \item{trial}{The trial number of the current trial}
+#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
+#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
+#'   "opposite" = the cue was presented opposite of the stimulus}
+#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
+#'   \item{CTI}{The cue target intercal coded in milliseconds.}
+#'   \item{stim}{The letter stimulus that was presented to be identified.}
+#'   \item{response}{The response given by the subject as the letter on the keyboard.}
+#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
+#'   \item{RTms}{Reaction Time of the response in milliseconds}
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"RawData_Exp2"
+
+#' Raw Data of the second Saccade Experiment
+#'
+#' @format ## `RawData_Exp3`
+#' A data frame with 35280 rows and 13 columns:
+#' \describe{
+#'   \item{Exp}{Label for the Experiment in the whole experiment series}
+#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
+#'   \item{Block}{The experimental block a trial was part of.}
+#'   \item{trial}{The trial number of the current trial}
+#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
+#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
+#'   "opposite" = the cue was presented opposite of the stimulus}
+#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
+#'   \item{SOA}{The stimulus onset asynchrony between cue and target onset in milliseconds.}
+#'   \item{CTI}{The cue target intercal coded in milliseconds.}
+#'   \item{stim}{The letter stimulus that was presented to be identified.}
+#'   \item{response}{The response given by the subject as the letter on the keyboard.}
+#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
+#'   \item{RTms}{Reaction Time of the response in milliseconds}
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"RawData_Exp3"
+
+#' Raw Data of the second Saccade Experiment
+#'
+#' @format ## `RawData_Exp4`
+#' A data frame with 35280 rows and 13 columns:
+#' \describe{
+#'   \item{Exp}{Label for the Experiment in the whole experiment series}
+#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
+#'   \item{Block}{The experimental block a trial was part of.}
+#'   \item{trial}{The trial number of the current trial}
+#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
+#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
+#'   "opposite" = the cue was presented opposite of the stimulus}
+#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
+#'   \item{SOA}{The stimulus onset asynchrony between cue and target onset in milliseconds.}
+#'   \item{CTI}{The cue target intercal coded in milliseconds.}
+#'   \item{stim}{The letter stimulus that was presented to be identified.}
+#'   \item{response}{The response given by the subject as the letter on the keyboard.}
+#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
+#'   \item{RTms}{Reaction Time of the response in milliseconds}
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"RawData_Exp4"
+
+#' Raw Data of the second Saccade Experiment
+#'
+#' @format ## `RawData_Exp5`
+#' A data frame with 35280 rows and 13 columns:
+#' \describe{
+#'   \item{Exp}{Label for the Experiment in the whole experiment series}
+#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
+#'   \item{Block}{The experimental block a trial was part of.}
+#'   \item{trial}{The trial number of the current trial}
+#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
+#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
+#'   "opposite" = the cue was presented opposite of the stimulus}
+#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
+#'   \item{SOA}{The stimulus onset asynchrony between cue and target onset in milliseconds.}
+#'   \item{CTI}{The cue target intercal coded in milliseconds.}
+#'   \item{stim}{The letter stimulus that was presented to be identified.}
+#'   \item{response}{The response given by the subject as the letter on the keyboard.}
+#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
+#'   \item{RTms}{Reaction Time of the response in milliseconds}
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"RawData_Exp5"
+
+#' Raw Data of the second Saccade Experiment
+#'
+#' @format ## `RawData_Exp6`
+#' A data frame with 35280 rows and 13 columns:
+#' \describe{
+#'   \item{Exp}{Label for the Experiment in the whole experiment series}
+#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
+#'   \item{Block}{The experimental block a trial was part of.}
+#'   \item{trial}{The trial number of the current trial}
+#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
+#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
+#'   "opposite" = the cue was presented opposite of the stimulus}
+#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
+#'   \item{CTI}{The cue target intercal coded in milliseconds.}
+#'   \item{stim}{The letter stimulus that was presented to be identified.}
+#'   \item{response}{The response given by the subject as the letter on the keyboard.}
+#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
+#'   \item{RTms}{Reaction Time of the response in milliseconds}
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"RawData_Exp6"
+
+#' Raw Data of the second Saccade Experiment
+#'
+#' @format ## `RawData_Exp7`
+#' A data frame with 35280 rows and 13 columns:
+#' \describe{
+#'   \item{Exp}{Label for the Experiment in the whole experiment series}
+#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
+#'   \item{Block}{The experimental block a trial was part of.}
+#'   \item{trial}{The trial number of the current trial}
+#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
+#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
+#'   "opposite" = the cue was presented opposite of the stimulus}
+#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
+#'   \item{CTI}{The cue target intercal coded in milliseconds.}
+#'   \item{stim}{The letter stimulus that was presented to be identified.}
+#'   \item{response}{The response given by the subject as the letter on the keyboard.}
+#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
+#'   \item{RTms}{Reaction Time of the response in milliseconds}
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"RawData_Exp7"
+
+#' Raw Data of the second Saccade Experiment
+#'
+#' @format ## `RawData_Exp8`
+#' A data frame with 35280 rows and 13 columns:
+#' \describe{
+#'   \item{Exp}{Label for the Experiment in the whole experiment series}
+#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
+#'   \item{Block}{The experimental block a trial was part of.}
+#'   \item{trial}{The trial number of the current trial}
+#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
+#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
+#'   "opposite" = the cue was presented opposite of the stimulus}
+#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
+#'   \item{CTI}{The cue target intercal coded in milliseconds.}
+#'   \item{stim}{The letter stimulus that was presented to be identified.}
+#'   \item{response}{The response given by the subject as the letter on the keyboard.}
+#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
+#'   \item{RTms}{Reaction Time of the response in milliseconds}
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"RawData_Exp8"
+
+#' Raw Data of the second Saccade Experiment
+#'
+#' @format ## `RawData_Exp9`
+#' A data frame with 35280 rows and 13 columns:
+#' \describe{
+#'   \item{Exp}{Label for the Experiment in the whole experiment series}
+#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
+#'   \item{Block}{The experimental block a trial was part of.}
+#'   \item{trial}{The trial number of the current trial}
+#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
+#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
+#'   "opposite" = the cue was presented opposite of the stimulus}
+#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
+#'   \item{CTI}{The cue target intercal coded in milliseconds.}
+#'   \item{stim}{The letter stimulus that was presented to be identified.}
+#'   \item{response}{The response given by the subject as the letter on the keyboard.}
+#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
+#'   \item{RTms}{Reaction Time of the response in milliseconds}
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"RawData_Exp9"
