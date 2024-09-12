a878f3601d9bd23c9002638d776dc0fa5f5496f8
Gidon Frischkorn <gidon.frischkorn@uzh.ch>
2024-09-12 11:25:16 +0200

    Update DataSet Names

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
-#' Raw Data of the first Saccade Experiment
+#' Raw Data of the first supplementary experiment in the Saccade Experiment Series
 #'
-#' @format ## `RawData_Exp1`
+#' @format ## `SuppExp1_data`
 #' A data frame with 25920 rows and 13 columns:
 #' \describe{
 #'   \item{Exp}{Label for the Experiment in the whole experiment series}
@@ -21,11 +21,11 @@
 #'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
 #'   \item{RTms}{Reaction Time of the response in milliseconds}
 #' }
-"RawData_Exp1"
+"SuppExp1_data"
 
-#' Raw Data of the second Saccade Experiment
+#' Raw Data of the second supplementary experiment in the Saccade Experiment Series
 #'
-#' @format ## `RawData_Exp2`
+#' @format ## `SuppExp2_data`
 #' A data frame with 35280 rows and 13 columns:
 #' \describe{
 #'   \item{Exp}{Label for the Experiment in the whole experiment series}
@@ -46,37 +46,11 @@
 #'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
 #'   \item{RTms}{Reaction Time of the response in milliseconds}
 #' }
-"RawData_Exp2"
+"SuppExp2_data"
 
-#' Raw Data of the third Saccade Experiment
+#' Raw Data of the third supplementary experiment in the Saccade Experiment Series
 #'
-#' @format ## `RawData_Exp3`
-#' A data frame with 35280 rows and 13 columns:
-#' \describe{
-#'   \item{Exp}{Label for the Experiment in the whole experiment series}
-#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
-#'   \item{Block}{The experimental block a trial was part of.}
-#'   \item{trial}{The trial number of the current trial}
-#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
-#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
-#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
-#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
-#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
-#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
-#'   "opposite" = the cue was presented opposite of the stimulus}
-#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
-#'   \item{SOA}{The stimulus onset asynchrony between cue and target onset in milliseconds.}
-#'   \item{CTI}{The cue target intercal coded in milliseconds.}
-#'   \item{stim}{The letter stimulus that was presented to be identified.}
-#'   \item{response}{The response given by the subject as the letter on the keyboard.}
-#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
-#'   \item{RTms}{Reaction Time of the response in milliseconds}
-#' }
-"RawData_Exp3"
-
-#' Raw Data of the fourth Saccade Experiment
-#'
-#' @format ## `RawData_Exp4`
+#' @format ## `SuppExp3_data`
 #' A data frame with 35280 rows and 13 columns:
 #' \describe{
 #'   \item{Exp}{Label for the Experiment in the whole experiment series}
@@ -98,11 +72,11 @@
 #'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
 #'   \item{RTms}{Reaction Time of the response in milliseconds}
 #' }
-"RawData_Exp4"
+"SuppExp3_data"
 
-#' Raw Data of the fifth Saccade Experiment (Individual Diff. Study 1)
+#' Raw Data of the fourth supplementary experiment in the Saccade Experiment Series
 #'
-#' @format ## `RawData_Exp5`
+#' @format ## `SuppExp4_data`
 #' A data frame with 35280 rows and 13 columns:
 #' \describe{
 #'   \item{Exp}{Label for the Experiment in the whole experiment series}
@@ -124,12 +98,12 @@
 #'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
 #'   \item{RTms}{Reaction Time of the response in milliseconds}
 #' }
-"RawData_Exp5"
+"SuppExp4_data"
 
-#' Raw Data of the sixth Saccade Experiment
+#' Raw Data of the first main experiment in the Saccade Experiment series (Individual Diff. Study 1)
 #'
-#' @format ## `RawData_Exp6`
-#' A data frame with 16200 rows and 13 columns:
+#' @format ## `Exp1_data`
+#' A data frame with 35280 rows and 13 columns:
 #' \describe{
 #'   \item{Exp}{Label for the Experiment in the whole experiment series}
 #'   \item{ID}{Unique integer code for each subject who participated in the experiment}
@@ -150,11 +124,37 @@
 #'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
 #'   \item{RTms}{Reaction Time of the response in milliseconds}
 #' }
-"RawData_Exp6"
+"Exp1_data"
 
-#' Raw Data of the seventh Saccade Experiment
+#' Raw Data of the fifth supplementary experiment in the Saccade Experiment Series
 #'
-#' @format ## `RawData_Exp7`
+#' @format ## `SuppExp5_data`
+#' A data frame with  rows and 14 columns:
+#' \describe{
+#'   \item{Exp}{Label for the Experiment in the whole experiment series}
+#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
+#'   \item{Block}{The experimental block a trial was part of.}
+#'   \item{trial}{The trial number of the current trial}
+#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
+#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
+#'   "opposite" = the cue was presented opposite of the stimulus}
+#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
+#'   \item{SOA}{The stimulus onset asynchrony between cue and target onset in milliseconds.}
+#'   \item{CTI}{The cue target intercal coded in milliseconds.}
+#'   \item{stim}{The letter stimulus that was presented to be identified.}
+#'   \item{response}{The response given by the subject as the letter on the keyboard.}
+#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
+#'   \item{RTms}{Reaction Time of the response in milliseconds}
+#' }
+"SuppExp5_data"
+
+#' Raw Data of the sixth supplementary experiment in the Saccade Experiment Series
+#'
+#' @format ## `SuppExp6_data`
 #' A data frame with 19440 rows and 14 columns:
 #' \describe{
 #'   \item{Exp}{Label for the Experiment in the whole experiment series}
@@ -176,11 +176,11 @@
 #'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
 #'   \item{RTms}{Reaction Time of the response in milliseconds}
 #' }
-"RawData_Exp7"
+"SuppExp6_data"
 
-#' Raw Data of the eight Saccade Experiment
+#' Raw Data of the seventh supplementary experiment in the Saccade Experiment Series
 #'
-#' @format ## `RawData_Exp8`
+#' @format ## `SuppExp7_data`
 #' A data frame with 21602 rows and 15 columns:
 #' \describe{
 #'   \item{Exp}{Label for the Experiment in the whole experiment series}
@@ -203,11 +203,11 @@
 #'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
 #'   \item{RTms}{Reaction Time of the response in milliseconds}
 #' }
-"RawData_Exp8"
+"SuppExp7_data"
 
-#' Raw Data of the ninth Saccade Experiment (Individual Diff. Study 2)
+#' Raw Data of the second main experiment in the Saccade Experiment series (Individual Diff. Study 2)
 #'
-#' @format ## `RawData_Exp9`
+#' @format ## `Exp2_data`
 #' A data frame with 126000 rows and 14 columns:
 #' \describe{
 #'   \item{Exp}{Label for the Experiment in the whole experiment series}
@@ -229,12 +229,12 @@
 #'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
 #'   \item{RTms}{Reaction Time of the response in milliseconds}
 #' }
-"RawData_Exp9"
+"Exp2_data"
 
 
 #' Raw Data of processing speed covariated from Experiment 9 (Individual Diff. Study 2)
 #'
-#' @format ## `Exp9_ProcSpeed`
+#' @format ## `Exp2_PS_data`
 #' A data frame with 47772 rows and 9 columns:
 #' \describe{
 #'   \item{task}{The task within Experiment 9 from which the data stem from}
@@ -247,11 +247,11 @@
 #'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
 #'   \item{RTms}{Reaction Time of the response in milliseconds}
 #' }
-"Exp9_ProcSpeed"
+"Exp2_PS_data"
 
 #' Raw Data of processing speed covariated from Experiment 9 (Individual Diff. Study 2)
 #'
-#' @format ## `Exp9_WorkingMemory`
+#' @format ## `Exp2_WMC_data`
 #' A data frame with 47160 rows and 13 columns:
 #' \describe{
 #'   \item{task}{The task within Experiment 9 from which the data stem from}
@@ -267,4 +267,4 @@
 #'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
 #'   \item{RTms}{Reaction Time of the response in milliseconds}
 #' }
-"Exp9_WorkingMemory"
+"Exp2_WMC_data"
