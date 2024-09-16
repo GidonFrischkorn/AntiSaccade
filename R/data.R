c32dc5771801f63c02fc2745efd0275e1e245f9b
Gidon Frischkorn <gidon.frischkorn@uzh.ch>
2024-09-16 14:57:17 +0200

    Updates

--- R/data.R
+++ R/data.R
@@ -48,7 +48,7 @@
 #' }
 "SuppExp2_data"
 
-#' Raw Data of the third supplementary experiment in the Saccade Experiment Series
+#' Raw Data of the fourth supplementary experiment in the Saccade Experiment Series
 #'
 #' @format ## `SuppExp3_data`
 #' A data frame with 35280 rows and 13 columns:
@@ -73,32 +73,6 @@
 #'   \item{RTms}{Reaction Time of the response in milliseconds}
 #' }
 "SuppExp3_data"
-
-#' Raw Data of the fourth supplementary experiment in the Saccade Experiment Series
-#'
-#' @format ## `SuppExp4_data`
-#' A data frame with 35280 rows and 13 columns:
-#' \describe{
-#'   \item{Exp}{Label for the Experiment in the whole experiment series}
-#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
-#'   \item{Block}{The experimental block a trial was part of.}
-#'   \item{trial}{The trial number of the current trial}
-#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
-#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
-#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
-#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
-#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
-#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
-#'   "opposite" = the cue was presented opposite of the stimulus}
-#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
-#'   \item{SOA}{The stimulus onset asynchrony between cue and target onset in milliseconds.}
-#'   \item{CTI}{The cue target intercal coded in milliseconds.}
-#'   \item{stim}{The letter stimulus that was presented to be identified.}
-#'   \item{response}{The response given by the subject as the letter on the keyboard.}
-#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
-#'   \item{RTms}{Reaction Time of the response in milliseconds}
-#' }
-"SuppExp4_data"
 
 #' Raw Data of the first main experiment in the Saccade Experiment series (Individual Diff. Study 1)
 #'
@@ -128,8 +102,34 @@
 
 #' Raw Data of the fifth supplementary experiment in the Saccade Experiment Series
 #'
-#' @format ## `SuppExp5_data`
+#' @format ## `SuppExp4_data`
 #' A data frame with  rows and 14 columns:
+#' \describe{
+#'   \item{Exp}{Label for the Experiment in the whole experiment series}
+#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
+#'   \item{Block}{The experimental block a trial was part of.}
+#'   \item{trial}{The trial number of the current trial}
+#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
+#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
+#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
+#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
+#'   "opposite" = the cue was presented opposite of the stimulus}
+#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
+#'   \item{SOA}{The stimulus onset asynchrony between cue and target onset in milliseconds.}
+#'   \item{CTI}{The cue target intercal coded in milliseconds.}
+#'   \item{stim}{The letter stimulus that was presented to be identified.}
+#'   \item{response}{The response given by the subject as the letter on the keyboard.}
+#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
+#'   \item{RTms}{Reaction Time of the response in milliseconds}
+#' }
+"SuppExp4_data"
+
+#' Raw Data of the sixth supplementary experiment in the Saccade Experiment Series
+#'
+#' @format ## `SuppExp5_data`
+#' A data frame with 19440 rows and 14 columns:
 #' \describe{
 #'   \item{Exp}{Label for the Experiment in the whole experiment series}
 #'   \item{ID}{Unique integer code for each subject who participated in the experiment}
@@ -152,35 +152,9 @@
 #' }
 "SuppExp5_data"
 
-#' Raw Data of the sixth supplementary experiment in the Saccade Experiment Series
-#'
-#' @format ## `SuppExp6_data`
-#' A data frame with 19440 rows and 14 columns:
-#' \describe{
-#'   \item{Exp}{Label for the Experiment in the whole experiment series}
-#'   \item{ID}{Unique integer code for each subject who participated in the experiment}
-#'   \item{Block}{The experimental block a trial was part of.}
-#'   \item{trial}{The trial number of the current trial}
-#'   \item{cueLoc}{The cue location coded as a numeric value from 1 to 4.
-#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
-#'   \item{stimLoc}{The stimulus location coded as a numeric value from 1 to 4.
-#'   1 = "top", 2 = "right", 3 = "bottom", 4 = "left"}
-#'   \item{locDist}{The distance between the cue and stimulus location coded as a factor,
-#'   "same" = same location for cue and stimulus, "close" = the cue was one spatial position away from the stimulus,
-#'   "opposite" = the cue was presented opposite of the stimulus}
-#'   \item{FixDur}{The fixation duration preceeding each trial in milliseconds.}
-#'   \item{SOA}{The stimulus onset asynchrony between cue and target onset in milliseconds.}
-#'   \item{CTI}{The cue target intercal coded in milliseconds.}
-#'   \item{stim}{The letter stimulus that was presented to be identified.}
-#'   \item{response}{The response given by the subject as the letter on the keyboard.}
-#'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
-#'   \item{RTms}{Reaction Time of the response in milliseconds}
-#' }
-"SuppExp6_data"
-
 #' Raw Data of the seventh supplementary experiment in the Saccade Experiment Series
 #'
-#' @format ## `SuppExp7_data`
+#' @format ## `SuppExp6_data`
 #' A data frame with 21602 rows and 15 columns:
 #' \describe{
 #'   \item{Exp}{Label for the Experiment in the whole experiment series}
@@ -203,7 +177,7 @@
 #'   \item{correct}{Logical variable indicating the response was correct (TRUE) or incorrect (FALSE)}
 #'   \item{RTms}{Reaction Time of the response in milliseconds}
 #' }
-"SuppExp7_data"
+"SuppExp6_data"
 
 #' Raw Data of the second main experiment in the Saccade Experiment series (Individual Diff. Study 2)
 #'
